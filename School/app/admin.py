d55c3e30267797620e6b94f343567af023e22544
Arshad Alam <aaalm59@gmail.com>
2023-11-15 12:49:29 +0530

    add setti- database

--- School/app/admin.py
+++ School/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# admin.py
+
+from django.contrib import admin
+from app.models import Teacher, Student, Course, Class
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'roll_number', 'email', 'phone_number', 'date_of_birth')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'description')
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'start_date', 'end_date')
+    filter_horizontal = ('teachers', 'students')
--- School/app/models.py
+++ School/app/models.py
@@ -1,3 +1,40 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    teachers = models.ManyToManyField(Teacher)
+    students = models.ManyToManyField(Student)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
